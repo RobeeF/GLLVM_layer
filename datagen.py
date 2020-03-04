fix(misc): misclassification error computation was erroneous

datagen.py
@@ -188,8 +188,8 @@ def gen_mvdata(z, init, seed = None):
 
 # Univariate data
 
-numobs = 1000
-M = 300
+numobs = 2000
+M = 100
 
 z, labels = gen_z(numobs, seed)
 y = gen_data(z, 1)
@@ -235,17 +235,19 @@ init = init_params(r, p, p1, p2, o, szo, k, 1)
 out = gllvm_alg_mc_pilot(y, numobs, r, k, p, p1, p2, 50, o, szo, init, eps, maxstep, 
                              var_distrib, nj, M, None)
 
-out['sigma']
+out['w']
 misc(labels, out['classes'])
+new_misc(labels, out['classes'])
 plt.scatter(labels, out['classes'])
 
 
 true = labels
 pred = out['classes']
 correct_classes = out['classes'] + 5
-correct_classes = np.where(correct_classes == 5, 2, correct_classes)
-correct_classes = np.where(correct_classes == 7, 0, correct_classes)
-correct_classes = np.where(correct_classes == 6, 1, correct_classes)
+correct_classes = np.where(correct_classes == 5, 1, correct_classes)
+correct_classes = np.where(correct_classes == 6, 0, correct_classes)
+correct_classes = np.where(correct_classes == 7, 2, correct_classes)
+misc(labels, correct_classes)
 
 
 print(np.vstack([labels, out['classes']]).T[:200])

misc.py
@@ -10,25 +10,29 @@ from copy import deepcopy
 from itertools import permutations
 
 def misc(true, pred):
+    ''' Compute a label invariant misclassification error '''
     best_misc = 0
-    true_classes = np.unique(true)
+    true_classes = np.unique(true).astype(int)
     nb_classes = len(true_classes)
     best_misc = 1
-
-    for i in range(nb_classes):
-        relabeled_pred = (pred + i) % nb_classes
-        current_misc = np.mean(true != relabeled_pred)
+    
+    # Compute of the possible labels
+    all_possible_labels = [list(l) for l in list(permutations(true_classes))]
+    
+    # And compute the misc for each label
+    for l in all_possible_labels:
+        shift = max(true_classes) + 1
+        shift_pred = pred + max(true_classes) + 1
+        
+        for i in range(nb_classes):
+            shift_pred = np.where(shift_pred == i + shift, l[i], shift_pred)
+        
+        current_misc = np.mean(true != shift_pred)
         if current_misc < best_misc:
             best_misc = deepcopy(current_misc)
     return best_misc
 
-i = 0
-correct_classes - relabeled_pred
 
-def new_misc(true, pred):
-    best_misc = 0
-    true_classes = np.unique(true)
-    nb_classes = len(true_classes)
-    best_misc = 1
+
+
     
-    all_possible_labels = list(permutations(true))